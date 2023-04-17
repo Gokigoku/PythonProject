e92758f4555ff117310e8784c19c9e972d615c69
Gokul kr <106290510+Gokigoku@users.noreply.github.com>
2023-04-18 00:19:45 +0530

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -15,8 +15,16 @@ def binery(li,s,l,h):
 li1 = []
 i = input("Enter the size of a list: ")
 print("enter the item :")
+m=0
 for _ in range(int(i)):
     li1.append(int(input()))
+for _ in range(len(li1)):
+    if li1[m] < li1[m+1]:
+        m += 1
+        continue
+    else:
+        print("Invalid Input")
+        exit()
 f = int(input("enter the searching element: "))
 low=0
 high=len(li1)-1
